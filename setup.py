dceef40c416eb3d3fdcc8bd5704671ba670580c5
biosckon <biosckon@hotmail.com>
2019-06-19 14:37:15 +0200

    adds auto increment to the version number x.x.inc(x)

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 from setuptools import setup
 
-setup(name='petpy',
-      version='0.2',
-      description='Petrophysics utilities',
-      url='https://example.com/',
-      author='Agile Scientific',
-      author_email='matt@agilescientific.com',
-      license='Apache 2',
-      packages=['petpy'],
-      install_requires=[
-          'numpy',
-          ],
-      )
+VERSION = ""
+
+with open("VERSION", "r+") as f:
+    content = f.read()
+    ver = str(content).strip().split(".")
+    ver[2] = str(int(ver[2]) + 1)
+    VERSION = ".".join(ver)
+    f.seek(0)
+    f.write(VERSION)
+
+
+setup(
+    name="petpy",
+    version=VERSION,
+    description="Petrophysics utilities",
+    url="https://example.com/",
+    author="Agile Scientific",
+    author_email="matt@agilescientific.com",
+    license="Apache 2",
+    packages=["petpy"],
+    install_requires=["numpy"],
+)
